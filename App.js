c3554cb0708e46dbd3f24009a8374aa4a3aad4e0
ARobinson92 <72640234+ARobinson92@users.noreply.github.com>
2021-01-26 14:41:58 -0800

    small changes

--- App.js
+++ App.js
@@ -6,7 +6,7 @@ import File from './components/file.js'
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>This is where our App info will go.</Text>
+      <Text style={styles.texttag}>This is where our App info will go.</Text>
       <File />
       <StatusBar style="auto" />
     </View>
@@ -16,8 +16,29 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'red',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  texttag: {
+    color: 'blue',
+    fontSize: 16,
+    padding: 35,
+    backgroundColor: 'black',
+    borderStyle: 'solid',
+    borderColor: 'black',
+    borderRadius: 75,
+    height: 150,
+    width: 150,
+    overflow: 'hidden',
+    alignItems: 'center',
+    justifyContent: 'space-evenly',
+  },
+  component: {
+    color: 'white',
+    fontWeight: 'bold',
+    fontSize: 25,
+    textDecorationStyle: 'solid',
+  }
 });
+
---
+++ app/components/file.js
@@ -0,0 +1,30 @@
+import * as React from 'react';
+import { StyleSheet, Text, View, Button, Alert } from 'react-native';
+
+export default function File() {
+    return (
+        <View>
+            <Text style={styles.component}>Or we can put in components, and give it it's own overriding stylesheet.</Text>
+            <Button 
+                style={styles.buttons}
+                title="We can add buttons, too!"
+                onPress={() => Alert.alert('Simple Button pressed')}
+            />
+        </View>
+    )   
+}
+
+const styles = StyleSheet.create({
+    component: {
+        color: 'white',
+        fontWeight: 'bold',
+        fontSize: 25,
+        textDecorationStyle: 'solid',
+        padding: 45,
+    }, 
+    buttons: {
+        padding: 15,
+        backgroundColor: 'lightblue',
+        borderColor: 'white',
+    }
+})
