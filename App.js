1125a307a05e5d1f23bf8dcaa99053f2a2601ff0
ARobinson92 <72640234+ARobinson92@users.noreply.github.com>
2021-01-25 17:16:33 -0800

    remove tsx

--- App.js
+++ App.js
@@ -1,13 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import File from './components/file.tsx'
+import File from './components/file.js'
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>This is where our App info will go.</Text>
       <h2>We can use html tags here,</h2>
+      <p>This is in a paragraph tag</p>
       <File />
       <StatusBar style="auto" />
     </View>
