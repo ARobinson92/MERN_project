032b1ee3d1af7f1de3334926623c5b4d3092f76d
ARobinson92 <72640234+ARobinson92@users.noreply.github.com>
2021-01-25 17:38:43 -0800

    added view tag

--- components/file.js
+++ components/file.js
@@ -1,5 +1,10 @@
 import * as React from 'react';
+import { Text, View } from 'react-native';
 
 export default function File() {
-  return <p>Or we can put in components.</p>;
+    return (
+        <View>
+            <Text>Or we can put in components.</Text>
+        </View>
+    )
 }
