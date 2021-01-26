94194669bbb64599106f893b8fa6c5be40371440
ARobinson92 <72640234+ARobinson92@users.noreply.github.com>
2021-01-25 16:43:39 -0800

    first commit

---
+++ App.js
@@ -0,0 +1,24 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import File from './components/file.tsx'
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>This is where our App info will go.</Text>
+      <h2>We can use html tags here,</h2>
+      <File />
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
