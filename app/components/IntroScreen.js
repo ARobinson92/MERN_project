71571243438d50b4e2abdfa370a9c5614176fd4d
ARobinson92 <72640234+ARobinson92@users.noreply.github.com>
2021-01-27 01:02:02 -0800

    Added styling components for desktop viewing

--- app/components/IntroScreen.js
+++ app/components/IntroScreen.js
@@ -1,6 +1,18 @@
 import React from 'react';
 import { Image, ImageBackground, StyleSheet, Text, View } from 'react-native';
 
+//Two attributes, bottomBox and paragraph, will need to be rendered differently when displayed on a 
+    //desktop computer. There is a separate style component for each in the stylesheet under
+    //bottomBoxDesktop and paragraphDesktop. The logic to make these appear when the site is 
+    //viewed from a desktop is in that tutorial from yesterday, but I haven't implemented it yet.
+
+//We also need to add functionality to the button by putting in an onPress function once the routing
+    //is in place, as it will need to route the user to the quiz component.
+
+//Also, I can't load an android emulator because my computer does not have the same power as a
+    //modern android phone. So, if someone could check to see that the site still looks okay,
+    //that would be dope.
+
 function IntroScreen() {
     return (
         <View>
@@ -9,10 +21,10 @@ function IntroScreen() {
             </View>
             <View style={styles.appBox}>
                 <Text style={styles.appName} numberOfLines={1}>Discovery</Text>
-                <Image source={require('../assets/vectorstock_27510217.png')} style = {{height:130, width:85}} />
+                <Image source={require('../assets/vectorstock_27510217.png')} style = {styles.logo} />
                 <Text style={styles.paragraph}>Inspired by the work of best-selling author Mark Manson, our goal is to help you uncover the values most important to you and use the power of reflection to acheive your goals.</Text>
             </View>
-            <View style={styles.middleBody}>
+            <View style={styles.bottomBox}>
                 <Text style={styles.button}>Begin</Text>
                 <Text style={styles.attribution}>Background Photo by Geran de Klerk on Unsplash</Text>
                 <Text style={styles.attribution}>Logo designed by litonmee (Image #27510217 at VectorStock.com)</Text>
@@ -50,10 +62,32 @@ const styles = StyleSheet.create({
         resizeMode: 'stretch',
         overflow: 'visible',
     },
-    middleBody: {
+    bottomBox: {
         flex: 1.6,
         justifyContent: 'flex-end',
         alignItems: 'center',
+    },
+    bottomBoxDesktop: {
+        flex: 1.6,
+        justifyContent: 'flex-end',
+        alignItems: 'center',
+        minHeight: 220,
+    },
+    button: {
+        backgroundColor: '#004972',
+        color: '#dad2cf',
+        bottom: 8,
+        width: '40%',
+        fontSize: 26,
+        textAlign: 'center',
+        borderRadius: 10,
+        overflow: 'hidden',
+        padding: 4,
+    },
+    logo: {
+        height:130, 
+        width:85, 
+        bottom: 10
     },
     paragraph: {
         color: '#004972',
@@ -67,14 +101,14 @@ const styles = StyleSheet.create({
         overflow: 'hidden',
         padding: 4,
     },
-    button: {
-        backgroundColor: '#004972',
-        color: '#dad2cf',
-        bottom: 8,
-        width: '40%',
-        fontSize: 26,
+    paragraphDesktop: {
+        color: '#004972',
+        fontSize: 13,
+        fontWeight: 'normal',
+        width: 285,
+        borderRadius: 20,
+        backgroundColor: 'rgba(218, 210, 207, 0.6)',
         textAlign: 'center',
-        borderRadius: 10,
         overflow: 'hidden',
         padding: 4,
     },
