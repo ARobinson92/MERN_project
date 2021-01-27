07792e72c1d5a354f3e46e10625d8334b20c943b
ARobinson92 <72640234+ARobinson92@users.noreply.github.com>
2021-01-26 23:01:56 -0800

    added styling to the intro screen

--- App.js
+++ App.js
@@ -1,13 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import File from './app/components/file.js'
+import IntroScreen from './app/components/IntroScreen.js'
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={styles.texttag}>This is where our App info will go.</Text>
-      <File />
+      {/* <Text style={styles.texttag}>This is where our App info will go.</Text>
+      <File /> */}
+      <IntroScreen />
       <StatusBar style="auto" />
     </View>
   );
@@ -15,8 +16,6 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: 'red',
     alignItems: 'center',
     justifyContent: 'center',
   },
--- app/components/IntroScreen.js
+++ app/components/IntroScreen.js
@@ -1,11 +1,22 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Image, ImageBackground, StyleSheet, Text, View } from 'react-native';
 
-function IntroScreen(props) {
+function IntroScreen() {
     return (
-        <View style={styles.appName}>
-            <Text>Application Name</Text>
-            
+        <View>
+            <View style={{height:0, flex:0}}>
+                <ImageBackground source={require('../assets/geran-de-klerk-Jmw2Ardu2YM-unsplash.jpg')} style = {styles.backgroundImage} />
+            </View>
+            <View style={styles.appBox}>
+                <Text style={styles.appName} numberOfLines={1}>Discovery</Text>
+                <Image source={require('../assets/vectorstock_27510217.png')} style = {{height:130, width:85}} />
+                <Text style={styles.paragraph}>Inspired by the work of best-selling author Mark Manson, our goal is to help you uncover the values most important to you and use the power of reflection to acheive your goals.</Text>
+            </View>
+            <View style={styles.middleBody}>
+                <Text style={styles.button}>Begin</Text>
+                <Text style={styles.attribution}>Background Photo by Geran de Klerk on Unsplash</Text>
+                <Text style={styles.attribution}>Logo designed by litonmee (Image #27510217 at VectorStock.com)</Text>
+            </View>
         </View>
     );
 }
@@ -13,10 +24,58 @@ function IntroScreen(props) {
 export default IntroScreen;
 
 const styles = StyleSheet.create({
+    appBox: {
+        flex: 2,
+        alignItems: 'center',
+        justifyContent: 'space-evenly',
+        borderRadius: 10,
+        top: 18,
+    },
     appName: {
-        fontSize: 36,
-        color: 'white',
-        backgroundColor: 'darkOliveGreen',
-        flex: 1,
-    }
+        fontSize: 43,
+        fontWeight: 'bold',
+        color: '#dad2cf',
+        alignItems: 'flex-start',
+        justifyContent: 'flex-start',
+    },
+    attribution: {
+        flex: 0,
+        fontSize: 8,
+        color: 'darkgrey',
+    },
+    backgroundImage: {
+        flex: 0, 
+        height: 568, 
+        width: 350,
+        resizeMode: 'stretch',
+        overflow: 'visible',
+    },
+    middleBody: {
+        flex: 1.6,
+        justifyContent: 'flex-end',
+        alignItems: 'center',
+    },
+    paragraph: {
+        color: '#004972',
+        fontSize: 18,
+        fontWeight: 'normal',
+        width: 285,
+        top: 12,
+        borderRadius: 20,
+        backgroundColor: 'rgba(218, 210, 207, 0.6)',
+        textAlign: 'center',
+        overflow: 'hidden',
+        padding: 4,
+    },
+    button: {
+        backgroundColor: '#004972',
+        color: '#dad2cf',
+        bottom: 8,
+        width: '40%',
+        fontSize: 26,
+        textAlign: 'center',
+        borderRadius: 10,
+        overflow: 'hidden',
+        padding: 4,
+    },
 })
