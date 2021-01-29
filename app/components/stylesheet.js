50e2f48e328f4f59f16481c64a7fb6d85e7f4baf
ARobinson92 <72640234+ARobinson92@users.noreply.github.com>
2021-01-29 10:43:19 -0800

    first commit

---
+++ app/components/stylesheet.js
@@ -0,0 +1,142 @@
+import React from 'react';
+import { StyleSheet } from "react-native";
+
+
+function stylesheet() {
+    
+const stylesheet = StyleSheet.create({
+        announcement: {
+            flex: 1,
+            alignItems: "center",
+        },
+        appBox: {
+            top: 18,
+            top: 20,
+            left: 10,
+            right: 10,
+            borderRadius: 10,
+            position: 'absolute',
+            alignItems: "center",
+            justifyContent: "space-evenly",
+        },
+        appName: {
+            padding: 10,
+            fontSize: 43,
+            color: "#dad2cf",
+            fontWeight: "bold",
+            alignItems: "flex-start",
+            justifyContent: "flex-start",
+        },
+        appPageContainer: {
+            top: 0,
+            left: 0,
+            right: 0,
+            bottom: 0,
+            position: "absolute",
+            justifyContent: "center",
+            backgroundColor: '#F6F4F4',
+            alignContent: "space-between",
+        },
+        attribution: {
+            fontSize: 8,
+            color: "darkgrey",
+        },
+        backgroundContainer: {
+            flex: 1,
+            resizeMode: "stretch",
+        },
+        backgroundImage: {
+            flex: 1,
+            resizeMode: "stretch",
+        },
+        bottomBox: {
+            left: 10,
+            right: 10,
+            bottom: 10,
+            alignItems: "center",
+            position: 'absolute',
+        },
+        button: {
+            margin: 8,
+            bottom: 12,
+            padding: 4,
+            width: "40%",
+            fontSize: 20,
+            color: "#51A0D5",
+            borderRadius: 10,
+            overflow: "hidden",
+            alignSelf: 'center',
+            textAlign: "center",
+            backgroundColor: "#004972",
+        },
+        container: {
+            flex: 9,
+            resizeMode: 'stretch',
+        },
+        content: {
+            top: 0,
+            left: 0,
+            right: 0,
+            bottom: 0,
+            position: 'absolute',
+        },
+        formContainer: {
+            flex: 2,
+            marginTop: 60,
+            marginLeft: 20,
+            position: 'relative',
+        },
+        header: {
+            top: 0,
+            left: 0,
+            right: 0,
+            padding:20,
+            fontSize: 32,
+            marginBottom:15,
+            shadowRadius:30,
+            color: '#F6F4F4',
+            shadowOpacity: 0.8,
+            shadowColor: 'grey',
+            position: 'absolute',
+            borderBottomWidth: 1,
+            backgroundColor: '#51A0D5',
+            borderBottomColor: 'black',
+        },
+        headerContainer: {
+            flex: 1,
+        },
+        logo: {
+            width: 85,
+            bottom: 10,
+            height: 130,
+        },
+        paragraph: {
+            top: 12,
+            width: 285,
+            padding: 4,
+            fontSize: 15,
+            color: "#004972",
+            borderRadius: 20,
+            overflow: "hidden",
+            textAlign: "center",
+            fontWeight: "normal",
+            backgroundColor: "rgba(218, 210, 207, 0.6)",
+        },
+        normal: {
+            flex: 1,
+            fontSize: 10,
+            color: '#0D0709',
+        },
+        radioform: {
+            color: '#0D0709',
+            position: 'relative',
+        },
+        textContainer: {
+            flex: 2,
+            marginLeft: 20,
+        },
+    });
+    return stylesheet;
+}
+
+export default stylesheet;
---
+++ app/screens/QuizScreen.jsx
@@ -0,0 +1,142 @@
+import React, { useState, useEffect } from "react";
+import {Alert, Text, View,} from "react-native";
+import RadioForm, {RadioButton, RadioButtonInput, RadioButtonLabel,} from "react-native-simple-radio-button";
+import wordList from "../components/wordList.js";
+import axios from "axios";
+import stylesheet from '../components/stylesheet.js'
+    //key=: 1fd93f78-0bc6-4fd2-b7f4-0e5b6124d23d
+function QuizScreen({navigation}) {
+    const [loaded, setLoaded] = useState(false);
+    const [complete, setComplete] = useState(false);
+    const [update, setUpdate] = useState(0);
+    const [values, setValues] = useState([]);
+    const [counter, setCounter] = useState(0);
+    const [pointers, setPointers] = useState([0, 1, 2, 3, 4]);
+    const [definitions, setDefinitions] = useState([]);
+    const WordList = wordList();
+    const styles = stylesheet();
+
+    useEffect(() => {
+        let temp = [];
+        for (var i = 0; i < WordList.length; i++) {
+            var word = WordList[i];
+            axios
+                .get(
+                    `https://www.dictionaryapi.com/api/v3/references/collegiate/json/${word}?key=1fd93f78-0bc6-4fd2-b7f4-0e5b6124d23d`
+                )
+                .then((res) => {
+                    var singleVal = {
+                        label: res.data[0].meta.id,
+                        value: res.data[0].shortdef,
+                        freq: 0,
+                    };
+                    temp.push(singleVal);
+                    if (temp.length >= WordList.length) {
+                        values.push(temp);
+                        setLoaded(true);
+                    }
+                })
+                .catch((err) => {
+                    console.log("There was an error: ", err);
+                    Alert.alert(
+                        "There was a problem loading the quiz. Please check your connection and try again."
+                    );
+                });
+        }
+    }, []);
+
+    const handlePress = (value) => { //Picks out the one they clicked on, setting its index in values[0] to 'update'.
+        setDefinitions(value);
+        for (let i = 0; i < values[0].length; i++) {
+            if (values[0][i]["value"] == value) {
+                setUpdate(i);
+                break;    
+            }
+        }
+    };
+
+    const buttonPress = () => {
+        setCounter(counter + 1);                    //Increments a counter every time a value is discarded
+        if (counter == values[0].length - 5) {      //If the counter gets to five less than the total number of words, 
+            setComplete(true);                      //The 'complete' state is set to TRUE, revealing the results section.
+        }
+        let tempArr = [...values[0]];               //Every time, the frequency value of update, which was set above, is incremented...
+        tempArr[update]["freq"]++;
+        values.pop();
+        values.push(tempArr);                       //...and returned to the values array.
+        let temp = [...pointers];
+        Alert.alert("Value : " + tempArr[update]["freq"]);  //An alert lets user know that the value was incremented
+        for (let i = 0; i < pointers.length; i++) { 
+            if (temp[i] == update) {                //Whichever pointer is pointing at the value the user selected...
+                for (let j = 0; j < values[0].length; j++) {
+                    if (!temp.includes(j) && values[0][j]["freq"] != 1) {
+                        temp[i] = j;                //Is set to the next value that is neither pointed at nor incremented
+                        i = pointers.length;
+                        break;
+                    }
+                }
+            }
+        }
+        setPointers(temp);                          //And returned to it's state in 'pointer'.
+    };
+
+    const returnHome = () => {
+        navigation.navigate("IntroScreen");
+    };
+
+    return (
+        <View style = {styles.appPageContainer}>
+            {complete ? (
+                <View style = {styles.appPageContainer}>
+                    <Text>THIS IS WHERE OUR RESULTS INFO WILL GO.</Text>
+                </View>
+            ):(<View style = {styles.appPageContainer}>    
+                    {loaded ? (
+                        <View style = {styles.appPageContainer}>
+                            <View style = {styles.headerContainer}>
+                                <Text style={styles.header}>
+                                    Choose the option you care about LEAST from the
+                                    values below:
+                                </Text>
+                            </View>
+                            <View style = {styles.formContainer}>
+                                <RadioForm style = {styles.RadioForm}
+                                    radio_props={[
+                                        values[0][pointers[0]],
+                                        values[0][pointers[1]],
+                                        values[0][pointers[2]],
+                                        values[0][pointers[3]],
+                                        values[0][pointers[4]],
+                                    ]}
+                                    initial={-1}
+                                    onPress={handlePress}
+                                />
+                            </View>
+                            <View style = {styles.normal}>
+                                {definitions.length != 0 ? (
+                                    <View>
+                                        <Text>Definition 1: {definitions[0]}</Text>
+                                        <Text>Definition 2: {definitions[1]}</Text>
+                                        <Text>Definition 3: {definitions[2]}</Text>
+                                    </View>
+                                ) : null}
+                            </View>
+                            <View>
+                                <Text onPress={buttonPress} style={styles.button}>
+                                    Continue
+                                </Text>
+                                <Text onPress={returnHome} style={styles.button}>
+                                    Home
+                                </Text>
+                            </View>
+                        </View>
+                    ) : (
+                        <Text style = {styles.announcement}>The quiz information is downloading.</Text>
+                    )}
+                </View>
+            )}
+        </View>
+    );
+}
+
+export default QuizScreen;
